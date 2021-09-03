5e43067412867f9870dc550b2defc09aab223cdb
JoelFrancisco <joelfranciscofilho@gmail.com>
2021-09-03 10:23:06 -0300

    adding routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { CreateUser } from './controllers/createUser';
+import { LoginUser } from "@controllers/loginUsers";
+import { VerifySessionId } from "@controllers/verifySessionId";
 
 const router = Router();
 
@@ -8,5 +10,7 @@ router.get('/test', (req, res) => {
 });
 
 router.post('/user', CreateUser.create);
+router.post('/verify', VerifySessionId.verify);
+router.post('/login', LoginUser.login);
 
 export { router };
