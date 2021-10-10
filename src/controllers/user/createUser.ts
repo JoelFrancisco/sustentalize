f1f639efb3c52fa36557408dd2320ab756785f10
JoelFrancisco <joelfranciscofilho@gmail.com>
2021-10-09 22:27:16 -0300

    fix: user creation

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -1,14 +1,10 @@
 import { Request, Response } from "express";
 import { PrismaClient, User } from "@prisma/client";
-// import { randomUUID } from 'crypto'; 
 import { config } from 'dotenv';
 config();
 
 import { UserRepository } from "../../repositories/implementation/PostgresUserRepository";
 import { BcryptPassword } from "../../utils/hash/Implementation/BcryptHashPassword";
-// import { MailOptions } from '../entities/MailOptions';
-// import { SenderCredentials } from '../entities/SenderCredentials';
-// import { Nodemailer } from '../providers/Mail/Implementation/Nodemailer';
 
 export class CreateUser {
   public static async create(req: Request, res: Response) {
@@ -33,26 +29,4 @@ export class CreateUser {
       await prisma.$disconnect();
     }
   }
-
-  // private static async handleEmail(user: User): Promise<boolean> {
-  //   const senderCredentials = new SenderCredentials(
-  //     process.env.SUSTENTALIZE_EMAIL_SERVICE!,
-  //     process.env.SUSTENTALIZE_EMAIL_USERNAME!,
-  //     process.env.SUSTENTALIZE_EMAIL_PASSWORD!,
-  //   );
-    
-  //   const mailOptions = new MailOptions(
-  //     process.env.SUSTENTALIZE_EMAIL_USERNAME!,
-  //     user.email,
-  //     `${user.username} ative sua conta no sustentalize`,
-  //     `<a href="">Clique no link para ativar sua conta</a>
-  //     <p>${user.activation_id}</p>
-  //     <p>Caso não tenha solicitado uma conta no sustentalize ignore esse email</p>
-  //     `,
-  //   );
-    
-  //   const mailProvider = new Nodemailer();
-  //   const mailStatus = await mailProvider.sendEmail(senderCredentials, mailOptions);
-  //   return mailStatus;
-  // }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { router } from './routes';
 
 const app = express();
@@ -7,6 +8,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
